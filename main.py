706acc3089fd4dcea9bc2975196e91542b75ff14
DeadRedmond <dead.redmond@gmail.com>
2021-02-08 00:13:24 +0200

    ban command testing

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ token = os.getenv("BOT_TOKEN")
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(status = discord.Status.idle, activity = discord.Game("Annihilation!"))
+    await bot.change_presence(status = discord.Status.online, activity = discord.Game("Annihilation!"))
     print("Online!")
 
 @bot.command()
@@ -20,23 +20,14 @@ async def ping(ctx):
 async def echo(ctx, *, arg):
     await ctx.send(arg)
 
-@bot.command(pass_context=True)
-@commands.has_role("test")
-async def check(ctx, user: discord.Member):
-    role = discord.utils.find(lambda r: r.name == 'test', ctx.message.server.roles)
-    if role in user.roles:
-        await ctx.send("у вас есть роль")
-    else:
-        await ctx.send("у вас нету роли")
-
 """Mass bans members with an optional delete_days parameter"""
-"""@bot.command()
-@commands.check_any(commands.is_owner(), is_guild_owner())
+@bot.command()
+@commands.has_permissions(ban_members=True)
 async def ban(ctx, members: commands.Greedy[discord.Member],
                    delete_days: typing.Optional[int] = 0, *,
                    reason: str):
     for member in members:
         await member.ban(delete_message_days=delete_days, reason=reason)
-"""
+@commands.ban.brief("Mass bans members with an optional delete_days parameter")
 
 bot.run(token)
