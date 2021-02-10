fc68db9d4d1f9feab5d038de8982312036ccd9c5
DeadRedmond <dead.redmond@gmail.com>
2021-02-10 15:12:07 +0200

    image search implemented

--- cogs/google.py
+++ cogs/google.py
@@ -4,6 +4,7 @@ import requests
 import urllib.parse
 import discord
 import json
+import random
 from discord.ext import commands
 from bs4 import BeautifulSoup
 
@@ -20,7 +21,6 @@ class Google(commands.Cog):
     @commands.command(aliases = ['google', 'search', 'гугл'])
     async def g(self, ctx, *, query):
         """Google web search"""
-        #input = "https://www.googleapis.com/customsearch/v1?q=" + urllib.parse.quote_plus(query) + "&start=1" + "&key=" + google_api_key + "&cx=" + custom_search_engine
         resp = requests.get("https://www.googleapis.com/customsearch/v1?q=" + urllib.parse.quote_plus(query) + "&start=1" + "&key=" + google_api_key + "&cx=" + custom_search_engine)
         if resp.status_code !=200:
             await ctx.send(f'{ctx.message.author.mention}:confused: Поиск невозможен, сервис не отвечает.')
@@ -33,27 +33,35 @@ class Google(commands.Cog):
             if len(result['items']) < 1:
                 return await ctx.send(f'{ctx.message.author.mention} :thinking: Интернет не в курсе, поищите что-то другое.')
             em = discord.Embed(color=0x992d22)
-            em.add_field(name='1. ' + result['items'][0]['title'], value='['+result['items'][0]['link']+']('+result['items'][0]['link']+')' ,inline=False)
-            em.add_field(name='2. ' + result['items'][1]['title'], value='['+result['items'][1]['link']+']('+result['items'][0]['link']+')' ,inline=False)
-            em.add_field(name='3. ' + result['items'][2]['title'], value='['+result['items'][2]['link']+']('+result['items'][0]['link']+')' ,inline=False)
+            for i in range(3):
+                em.add_field(name=i+1 + '. ' + result['items'][i]['title'], value='['+result['items'][i]['link']+']('+result['items'][i]['link']+')' ,inline=False)
+            #em.add_field(name='1. ' + result['items'][0]['title'], value='['+result['items'][0]['link']+']('+result['items'][0]['link']+')' ,inline=False)
+            #em.add_field(name='2. ' + result['items'][1]['title'], value='['+result['items'][1]['link']+']('+result['items'][0]['link']+')' ,inline=False)
+            #em.add_field(name='3. ' + result['items'][2]['title'], value='['+result['items'][2]['link']+']('+result['items'][0]['link']+')' ,inline=False)
             
             await ctx.send(f'{ctx.message.author.mention}, вот что мне удалось найти:', embed=em)
         
-        
-        '''searchInput = "https://google.com/search?q="+urllib.parse.quote(query)
-        res = requests.get(searchInput)
-        res.raise_for_status()
-        soup = BeautifulSoup(res.text, "html.parser")
-        linkElements = soup.select('div#main > div > div > div > a')
-        if len(linkElements) == 0:
-            await ctx.send("Couldn't find any results...")
-        else:
-            link = linkElements[0].get("href")
-            i = 0
-            while link[0:4] != "/url" or link[14:20] == "google":
-                i += 1
-                link = linkElements[i].get("href")
-            await ctx.send(":desktop: http://google.com"+link)'''
+    @commands.command(aliases = ['image', 'img', 'картинка'])
+    async def i(self, ctx, *, query):
+        """Image web search"""
+        resp = requests.get("https://www.googleapis.com/customsearch/v1?q=" + urllib.parse.quote_plus(query) + "&start=1" + "&key=" + google_api_key + "&cx=" + custom_search_engine + "&searchType=image")
+        if resp.status_code !=200:
+            await ctx.send(f'{ctx.message.author.mention}:confused: Поиск невозможен, сервис не отвечает.')
+        else: 
+            result = json.loads(resp.text)
+            try:
+                result['items']
+            except:
+                return await ctx.send(f'{ctx.message.author.mention} :thinking: Интернет не в курсе, поищите что-то другое.')
+            amount = result['items']
+            if amount < 1:
+                return await ctx.send(f'{ctx.message.author.mention} :thinking: Интернет не в курсе, поищите что-то другое.')
+            em = discord.Embed(color=0x992d22)
+            item = random.randint(0, amount)
+            em.set_image(url=result['items'][item]['link'])
+            em.set_footer(text="Запрос: \"" + query + "\"")
+            await ctx.send(f'{ctx.message.author.mention}, вот что мне удалось найти:', embed=em)
+
 
 
 #setup function
