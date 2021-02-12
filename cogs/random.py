253e01b45b316ab82f8d83a019573e4a033b1252
DeadRedmond <dead.redmond@gmail.com>
2021-02-12 03:52:14 +0200

    random meme add

--- cogs/random.py
+++ cogs/random.py
@@ -2,6 +2,7 @@
 import aiohttp
 import json
 import discord
+from random import randint
 from discord.ext import commands
 
 class Random(commands.Cog):
@@ -11,21 +12,23 @@ class Random(commands.Cog):
     
     def __init__(self, bot):
         self.bot = bot
+        self.header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+
 
     async def randomimageapi(self, ctx, url: str):
-        session = aiohttp.ClientSession()
-        async with session.get(url) as content:
-            if content.status != 200:
-                return await ctx.reply(":confused: Сервис не отвечает.")
-            res = json.loads(await content.text())
-            try:
-                res['link']
-            except:
-                print(f"Error in:\n {content.text}\n\n")
-                return await ctx.reply(":confused: Что-то пошло не так.")
-            em = discord.Embed(color=0xa0cfe5)
-            em.set_image(url=res['link'])
-            await ctx.send("", embed=em)
+        async with aiohttp.ClientSession(headers=self.header) as session:            
+            async with session.get(url) as content:
+                if content.status != 200:
+                    return await ctx.reply(":confused: Сервис не отвечает.")
+                res = json.loads(await content.text())
+                try:
+                    res['link']
+                except:
+                    print(f"Error in:\n {content.text}\n\n")
+                    return await ctx.reply(":confused: Что-то пошло не так.")
+                em = discord.Embed(color=0xa0cfe5)
+                em.set_image(url=res['link'])
+                await ctx.send("", embed=em)
 
 
 
@@ -49,6 +52,15 @@ class Random(commands.Cog):
         """ Posts a random fox """
         await self.randomimageapi(ctx, 'https://some-random-api.ml/img/fox')
 
+    @commands.command(aliases=['мем'])
+    async def meme(self, ctx):
+        """Posts a random meme"""
+        async with aiohttp.ClientSession(headers=self.header) as cs:
+            async with cs.get('https://www.reddit.com/r/memes/new.json?sort=hot') as res:
+                r = await res.json()
+                em = discord.Embed(title="Random meme", color=0xa0cfe5)
+                em.set_image(url=r['data']['children'][randint(0, 25)]['data']['url'])
+                await ctx.send("", embed=em)
 
 
 #setup function
