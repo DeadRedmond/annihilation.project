d1b8e12a37654d744ed444f24fb50bcb34821f4b
Roman Kovalenko <50929573+DeadRedmond@users.noreply.github.com>
2021-02-17 05:58:36 +0200

    Пропустил await

--- cogs/anime.py
+++ cogs/anime.py
@@ -69,7 +69,7 @@ class Anime(commands.Cog):
         hug
         """
         if user is not None:
-            nekoslifeapi(ctx, 'https://nekos.life/api/v2/img/hug', f'{ctx.author} крепко обнял {user}')
+            await nekoslifeapi(ctx, 'https://nekos.life/api/v2/img/hug', f'{ctx.author} крепко обнял {user}')
         else:
             await nekoslifeapi(ctx, 'https://nekos.life/api/v2/img/hug')
     
