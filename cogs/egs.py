605a42821eab189894169709748cfcc9abf08696
DeadRedmond <dead.redmond@gmail.com>
2021-02-19 18:34:08 +0200

    добавлено подобие описания

--- cogs/egs.py
+++ cogs/egs.py
@@ -7,9 +7,6 @@ from discord.ext import commands
 from datetime import datetime
 
 class EGS(commands.Cog):
-    '''
-    Получить еженедельную халяву из EGS
-    '''
 
     def __init__(self, bot):
         self.bot = bot
@@ -17,6 +14,9 @@ class EGS(commands.Cog):
 
     @commands.command(aliases=['егс'])
     async def egs(self, ctx):
+    '''
+    Получить еженедельную халяву из EGS
+    '''
         now=datetime.now()
         async with aiohttp.ClientSession() as session:
             async with session.get('https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions') as resp:
@@ -27,7 +27,7 @@ class EGS(commands.Cog):
                     await ctx.send("На этой неделе у нас следующие игры:")
                     for item in result['data']['Catalog']['searchStore']['elements']:
                         if now > datetime.strptime(item['effectiveDate'], '%Y-%m-%dT%H:%M:%S.%fZ'):
-                            em = discord.Embed(title=item['title'], url=f"https://www.epicgames.com/store/ru/product/{item['productSlug']}/home" ,color=0xa0cfe5)
+                            em = discord.Embed(title=item['title'], url=f"https://www.epicgames.com/store/ru/product/{item['productSlug']}/home", descriptiom=item['description'], color=0xa0cfe5)
                             em.set_image(url=item['keyImages'][2]['url'])
                             await ctx.send("", embed=em)
                         else:
