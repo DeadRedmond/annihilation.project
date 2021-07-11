25840aa4bb71e3cdfc6e6ce93d3c26e6e863fdc7
DeadRedmond <dead.redmond@gmail.com>
2021-07-11 16:36:21 +0300

    задана функция 'main'

---
+++ run.py
@@ -0,0 +1,33 @@
+#import
+import os
+import discord
+from discord.ext import commands
+from pretty_help import PrettyHelp
+
+from settings import token, google_api_key, custom_search_engine
+
+def main():
+    bot = commands.Bot(command_prefix=".", help_command=PrettyHelp(
+        active_time=120,
+        color=0xa0cfe5))
+
+
+    #enable cogs
+    for extension in os.listdir("cogs"):
+        if extension.endswith(".py"):
+            try:
+                bot.load_extension("cogs." + extension[:-3])
+            except Exception as e:
+                print('Failed to load extension {}\n{}: {}'.format(extension, type(e).__name__, e))
+
+    #events
+    @bot.event
+    async def on_ready():
+        await bot.change_presence(status = discord.Status.online, activity = discord.Game(".help"))
+        print("Online!")
+
+    #bot run
+    bot.run(token)
+
+if __name__ == "__main__":
+	main()
